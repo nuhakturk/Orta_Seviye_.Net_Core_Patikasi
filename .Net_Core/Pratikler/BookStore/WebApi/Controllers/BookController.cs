46884f6b31197df3f749bd3ab2fe3e0dba5c0468
Nuh Akturk <nuhar42@gmail.com>
2022-09-16 19:09:13 +0300

    Delete endpointinin yazilmasi

--- .Net_Core/Pratikler/BookStore/WebApi/Controllers/BookController.cs
+++ .Net_Core/Pratikler/BookStore/WebApi/Controllers/BookController.cs
@@ -84,5 +84,18 @@ namespace WebApi.Controllers
 
             return Ok();
         }
+
+        // Delete
+        [HttpDelete("{id}")]
+        public IActionResult DeleteBook(int id)
+        {
+            var book = BookList.SingleOrDefault(x => x.Id == id);
+            if (book is null)
+            {
+                return BadRequest();
+            }
+            BookList.Remove(book);
+            return Ok();
+        }
     }
 }

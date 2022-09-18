e39d01204ea18f0bb23237038515ebabf275ab1a
Nuh Akturk <nuhar42@gmail.com>
2022-09-18 04:41:06 +0300

    Auto Increment ID kolonunun eklenmesi dersi uyg

--- .Net_Core/Pratikler/BookStore/WebApi/DBOperations/DataGenerator.cs
+++ .Net_Core/Pratikler/BookStore/WebApi/DBOperations/DataGenerator.cs
@@ -17,21 +17,21 @@ namespace WebApi.DBOperations
                 }
                 context.Books.AddRange(
                     new Book{
-                        Id = 1,
+                        //Id = 1,
                         Title = "Lean Startup",
                         GenreId = 1, // Personal Growth
                         PageCount = 200,
                         PublishDate = new DateTime(2001,06,12)
                     },
                     new Book{
-                        Id = 2,
+                        //Id = 2,
                         Title = "Herland",
                         GenreId = 2, // Science Fiction
                         PageCount = 250,
                         PublishDate = new DateTime(2010,05,23)
                     },
                     new Book{
-                        Id = 3,
+                        //Id = 3,
                         Title = "Dune",
                         GenreId = 2, // Science Fiction
                         PageCount = 540,

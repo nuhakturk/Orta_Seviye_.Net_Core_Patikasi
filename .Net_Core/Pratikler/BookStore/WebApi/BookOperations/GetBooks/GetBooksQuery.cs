10e69b1f8d6e4f0be9f2fe0ed9337b5ffc650695
Nuh Akturk <nuhar42@gmail.com>
2022-10-13 18:03:15 +0300

    Model kullanimi dersi uygulama

--- .Net_Core/Pratikler/BookStore/WebApi/BookOperations/GetBooks/GetBooksQuery.cs
+++ .Net_Core/Pratikler/BookStore/WebApi/BookOperations/GetBooks/GetBooksQuery.cs
@@ -1,7 +1,6 @@
-using System;
 using System.Collections.Generic;
 using System.Linq;
-using Microsoft.AspNetCore.Mvc;
+using WebApi.Common;
 using WebApi.DBOperations;
 
 namespace WebApi.BookOperations.GetBooks
@@ -9,10 +8,35 @@ namespace WebApi.BookOperations.GetBooks
     public class GetBooksQuery
     {
         
-        private readonly BooksStoreDbContext _dbContext;
-        public GetBooksQuery(BooksStoreDbContext dbContext)
+        private readonly BookStoreDbContext _dbContext;
+        public GetBooksQuery(BookStoreDbContext dbContext)
         {
             _dbContext = dbContext;
         }
+
+        public List<BooksViewModel> Handle()
+        {
+            var bookList = _dbContext.Books.OrderBy(x => x.Id).ToList<Book>();
+            List<BooksViewModel> vm = new List<BooksViewModel>();
+            foreach (var book in bookList)
+            {
+                vm.Add(new BooksViewModel()
+                {
+                    Title = book.Title,
+                    Genre = ((GenreEnum)book.GenreId).ToString(),
+                    PublishDate = book.PublishDate.Date.ToString("dd/MM/yyy"),
+                    PageCount = book.PageCount
+                });
+            }
+            return vm;
+        }
+    }
+
+    public class BooksViewModel
+    {
+        public string Title { get; set; }
+        public int PageCount { get; set; }
+        public string PublishDate { get; set; }
+        public string Genre { get; set; }
     }
 }
--- .Net_Core/Pratikler/BookStore/WebApi/Controllers/BookController.cs
+++ .Net_Core/Pratikler/BookStore/WebApi/Controllers/BookController.cs
@@ -2,7 +2,10 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.AspNetCore.Mvc;
+using WebApi.BookOperations.CreateBook;
+using WebApi.BookOperations.GetBooks;
 using WebApi.DBOperations;
+using static WebApi.BookOperations.CreateBook.CreateBookCommand;
 
 namespace WebApi.AddControllers
 {
@@ -18,10 +21,11 @@ namespace WebApi.AddControllers
         
 
         [HttpGet]
-        public List<Book> GetBooks()
+        public IActionResult GetBooks()
         {
-            var bookList = _context.Books.OrderBy(x => x.Id).ToList<Book>();
-            return bookList;
+            GetBooksQuery query = new GetBooksQuery(_context);
+            var result = query.Handle();
+            return Ok(result);
         }
 
         [HttpGet("{id}")]
@@ -40,15 +44,18 @@ namespace WebApi.AddControllers
 
         //Post
         [HttpPost]
-        public IActionResult AddBook([FromBody] Book newBook)
+        public IActionResult AddBook([FromBody] CreateBookModel newBook)
         {
-            var book = _context.Books.SingleOrDefault(x => x.Title == newBook.Title);
-            if (book is not null)
+            CreateBookCommand command = new CreateBookCommand(_context);
+            try
             {
-                return BadRequest();
+                command.Model = newBook;
+                command.Handle();
             }
-            _context.Books.Add(newBook);
-            _context.SaveChanges();
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
             return Ok();
         }
 
